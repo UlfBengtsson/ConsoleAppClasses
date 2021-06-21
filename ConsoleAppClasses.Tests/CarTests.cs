be0b80b286192aea1fa1b4a65dfb9e839500f492
Ulf Bengtsson <Ulf.Bengtsson.Lexicon@outlook.com>
2021-06-21 14:25:22 +0200

    added some more test

--- ConsoleAppClasses.Tests/CarTests.cs
+++ ConsoleAppClasses.Tests/CarTests.cs
@@ -22,13 +22,64 @@ namespace ConsoleAppClasses.Tests
             string color = "blue";
             int year = 1987;
             int hp = 210;
-            Car testCar;
+            Car testCar = null;
 
             //Act
             testCar = new Car(brand, model, color, year, hp);
 
             //Assert
+            Assert.NotNull(testCar);
+            Assert.Equal(brand, testCar.brand);
+            Assert.Equal(model, testCar.Model);
+            Assert.Equal(color, testCar.color);
+            Assert.Equal(year, testCar.year);
+            Assert.Equal(hp, testCar.hp);
+        }
 
+        [Theory]
+        [InlineData("900")]
+        [InlineData("900S")]
+        [InlineData("9-5")]
+        [InlineData("9-X")]
+        [InlineData("Kadett")]
+        [InlineData("X3")]
+        [InlineData("Diablo")]
+        [InlineData("Model 3")]
+        public void ModelPropValidInputs(string modelInput)
+        {
+            //Arrange
+            string brand = "Saab";
+            string color = "blue";
+            int year = 1987;
+            int hp = 210;
+            Car testCar = null;
+
+            //Act
+            testCar = new Car(brand, modelInput, color, year, hp);
+
+            //Assert
+            Assert.Equal(modelInput, testCar.Model);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+
+        public void ModelPropBadInputs(string modelInput)
+        {
+            //Arrange
+            string brand = "Saab";
+            string color = "blue";
+            int year = 1987;
+            int hp = 210;
+
+            //Act
+            ArgumentException result = Assert.Throws<ArgumentException>(
+                () => new Car(brand, modelInput, color, year, hp));
+
+            //Assert
+            Assert.Equal("Model cant be empty.", result.Message);
         }
     }
 }
