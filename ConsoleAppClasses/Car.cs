7e5f90c61dedb19729f94a3edfe60c5184db2137
Ulf Bengtsson <Ulf.Bengtsson.Lexicon@outlook.com>
2021-06-21 11:56:42 +0200

    Add project files.

---
+++ ConsoleAppClasses/Car.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppClasses
+{
+    public class Car
+    {
+        //default access modifier is private inside a class
+
+        static int idCounter = 0;
+        public static int IdCounter { get { return idCounter; } }
+
+        readonly int id;
+        public int Id { get { return id; } }
+
+
+        public readonly string brand;//field
+        private string model;
+        public string Model//Prop
+        {
+            get 
+            {
+                return model; 
+            }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Model cant be empty.");
+                }
+                model = value;
+            }
+        }
+        public string color;
+        public int year;
+        public int hp;
+
+        //if you dont make a constructor, C# will add a zero cunstructor for you.
+
+        public Car(string brand, string carModel, string color, int year, int hp)
+        {
+            id = ++idCounter;
+            this.brand = brand;
+            Model = carModel;
+            this.color = color;
+            this.year = year;
+            this.hp = hp;
+        }
+
+        public string Info()
+        {
+            return $"---- Car Info ----\nId: {id}\nBrand: {brand}\nModel: {model}\nColor: {color}\nYear: {year}\nBHP: {hp}";
+        }
+
+        public override string ToString()
+        {
+            return Info();
+        }
+    }
+}
---
+++ ConsoleAppClasses/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleAppClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Car id counter: " + Car.IdCounter);
+
+            Car myCar = new Car("Saab","900S","Dark blue",1989, 320);
+            Console.WriteLine("Car id counter: " + Car.IdCounter);    
+            
+            Car anotherCar = new Car("Volvo","740","Brun",1981, 120);
+            Console.WriteLine("Car id counter: " + Car.IdCounter);
+            
+            Car someonceCar = new Car("BMW","525i","Black",2001, 420);
+            Console.WriteLine("Car id counter: " + Car.IdCounter);
+            
+
+            //Console.WriteLine(myCar.brand);
+
+            //Console.WriteLine(myCar.Model);
+
+            //myCar.Model = "9000";
+
+            //Console.WriteLine(myCar.ToString());//Console.WriteLine(myCar);
+
+            Console.WriteLine(myCar.Info());
+            Console.WriteLine(anotherCar.Info());
+            Console.WriteLine(someonceCar.Info());
+
+        }
+    }
+}
